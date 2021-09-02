7094c814de75bf13b24ad1e1f245946f3cb20235
Arpon7 <arponbose2810@gmail.com>
2021-09-02 09:53:19 +0600

    matched search naumber added

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
 
+const searchInput = document.getElementById('search-input');
+const searchBtn = document.getElementById('search-btn');
+searchBtn.addEventListener('click', captureInput);
+
+function captureInput() {
+  const search = searchInput.value;
+  const url = `http://openlibrary.org/search.json?q=${search}`;
+
+  fetch(url)
+    .then(res => res.json())
+    .then((data) => displaySearch(data))
+
+}
+
+function displaySearch(data) {
+  {
+
+    //display matched items number
+    console.log(data.numFound);
+    const matched = data.numFound;
+    const div = document.getElementById('number-div');
+    const p = document.createElement('p');
+    p.innerText = `number of matched book: ${matched}`;
+    div.appendChild(p);
+
+
+
+    console.log(data.docs)
+    data.docs.forEach(item => {
+      console.log(item.title)
+    })
+
+  }
+}
