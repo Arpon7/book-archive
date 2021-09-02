899ba78a3e2682a42b54a5fa7a1894ce642701d2
Arpon7 <arponbose2810@gmail.com>
2021-09-02 12:42:28 +0600

    book name and author added

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ searchBtn.addEventListener('click', captureInput);
 
 function captureInput() {
   const search = searchInput.value;
+  searchInput.value = "";
   const url = `http://openlibrary.org/search.json?q=${search}`;
 
   fetch(url)
@@ -14,22 +15,40 @@ function captureInput() {
 }
 
 function displaySearch(data) {
-  {
+  console.log(data)
 
-    //display matched items number
-    console.log(data.numFound);
-    const matched = data.numFound;
-    const div = document.getElementById('number-div');
-    const p = document.createElement('p');
-    p.innerText = `number of matched book: ${matched}`;
-    div.appendChild(p);
+  //display matched items number
+  console.log(data.numFound);
+  const matched = data.numFound;
+  const div = document.getElementById('number-div');
+  const p = document.createElement('p');
+  p.innerText = `number of matched book: ${matched}`;
+  div.appendChild(p);
 
 
+  const searchResult = document.getElementById('search-result');
 
-    console.log(data.docs)
-    data.docs.forEach(item => {
-      console.log(item.title)
-    })
+  data.docs.forEach(book => {
+    console.log(book)
+    const bookTitle = book.title;
+    const bookAuthor = book.author_name;
 
-  }
+    const div = document.createElement('div');
+    div.classList.add('col');
+    div.innerHTML = `
+    <div class="card">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h3 class="card-title">${bookTitle}</h3>
+            <h6>${bookAuthor}</h6>
+            <p>${}</p>
+
+            
+          </div>
+        </div>
+    `
+    searchResult.appendChild(div)
+  })
+
+
 }
